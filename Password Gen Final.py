e9a522798901654977071181f690c10922c3d7ec
pogdogpy <101480097+pogdogpy@users.noreply.github.com>
2023-02-02 15:15:34 +0000

    Update Password Gen Final.py
    fixed fuckers

--- Password Gen Final.py
+++ Password Gen Final.py
@@ -15,16 +15,20 @@ while True:
          self.password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(self.length))
          return self.password
     
-if __name__ == '__main__':
+  if __name__ == '__main__':
     length = int(input('Enter the length of the password: '))
     random_pass(length)
 
 
+    a = input("Do you want to gen another password? y/n ")
 
-a = input("Do you want to gen another password? y/n ")
+  if a == "y":
+        continue
+  else:
+        break
 
-if a == "y":
-    continue
-else:
-    break
+
+
+
+
 
