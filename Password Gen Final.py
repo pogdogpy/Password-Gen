a96d7d057c396efe296348942ac0a1ad72db3854
pogdogpy <101480097+pogdogpy@users.noreply.github.com>
2023-02-02 15:00:27 +0000

    Update Password Gen Final.py
    k.dot fix pls

--- Password Gen Final.py
+++ Password Gen Final.py
@@ -1,15 +1,30 @@
 import string
 import random
 
-class random_pass:
-    def __init__(self, length) -> None:
-        self.length = length
-        print(f'Your password is {self.main()}')
+
+
+while True:
+
+
+  class random_pass:
+     def __init__(self, length) -> None:
+         self.length = length
+         print(f'Your password is {self.main()}')
         
-    def main(self) -> str:
-        self.password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(self.length))
-        return self.password
+     def main(self) -> str:
+         self.password = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(self.length))
+         return self.password
     
 if __name__ == '__main__':
     length = int(input('Enter the length of the password: '))
     random_pass(length)
+
+
+
+a = input("Do you want to gen another password? y/n ")
+
+if a == "y":
+    continue
+else:
+    break
+
